c3070cf8c7e0ec9823e726292999a73989c6d828
filipmatic <filip.matic.6@gmail.com>
2018-09-13 22:36:57 -0400

    set up of Kontakt project and workspace

---
+++ Kontakt/Kontakt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kontakt
+//
+//  Created by Filip Matić on 2018-09-13.
+//  Copyright © 2018 Filip Matić. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
