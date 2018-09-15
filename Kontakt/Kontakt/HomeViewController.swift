f700665b298ead8545bd0102b10432d9e517b2f2
filipmatic <filip.matic.6@gmail.com>
2018-09-15 14:20:34 -0400

    rename ViewController and add background colour

--- Kontakt/Kontakt/HomeViewController.swift
+++ Kontakt/Kontakt/HomeViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.brown
     }
 
     override func didReceiveMemoryWarning() {
