412abb23596f103d3cddac5959573e63717f1093
filipmatic <filip.matic.6@gmail.com>
2018-10-08 22:50:23 -0400

    Bring up camera view

--- Kontakt/Kontakt/ScannerViewController.swift
+++ Kontakt/Kontakt/ScannerViewController.swift
@@ -16,16 +16,51 @@ class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDel
     
     @IBOutlet weak var cameraView: UIView!
     
-//    @IBAction func HomeScreenButton(_ sender: Any)
-//    {
-//        dismiss(animated: true, completion: nil)
-//    }
+    @IBAction func HomeScreenButton(_ sender: Any)
+    {
+        dismiss(animated: true, completion: nil)
+    }
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.init(red: 153.0/255.0, green: 204.0/255.0, blue: 240.0/255.0, alpha: 1.0)
-        cameraView.backgroundColor = .clear
+//        cameraView.backgroundColor = .black
+//
+//        print("entering")
+//
+//        //Create session
+//        let session = AVCaptureSession()
+//        //Capture device
+//        let captureDevice = AVCaptureDevice.default(for: .video)
+//
+//        do
+//        {
+//            let input = try AVCaptureDeviceInput(device: captureDevice!)
+//            session.addInput(input)
+//        }
+//        catch
+//        {
+//            print("error")
+//        }
+//
+//        let output = AVCaptureMetadataOutput()
+//        session.addOutput(output)
+//        output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+//        output.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+//        video = AVCaptureVideoPreviewLayer(session: session)
+//        video.frame = cameraView.layer.bounds
+////        video.frame = self.view.layer.bounds
+//        cameraView.layer.addSublayer(video)
+//
+//
+//        print("running session")
+//        session.startRunning()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        cameraView.backgroundColor = .black
         
         print("entering")
         
@@ -49,14 +84,27 @@ class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDel
         output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
         output.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
         video = AVCaptureVideoPreviewLayer(session: session)
-//        video.frame = cameraView.layer.bounds
-        video.frame = self.view.layer.bounds
+        video.frame = cameraView.layer.bounds
+        //        video.frame = self.view.layer.bounds
         cameraView.layer.addSublayer(video)
         
         
         print("running session")
-//        session.startRunning()
+        session.startRunning()
     }
-
-
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        if (metadataObjects != nil) && (metadataObjects.count != 0) {
+            if let object = metadataObjects[0] as AVMetadataMachineReadableCodeObject {
+                if object.type == AVMetadataObject.ObjectType.qr {
+                    let alert = UIAlertController(title: "Your code is:", message: object.stringValue, preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Retake", style: .default, handler: nil))
+                    alert.addAction(UIAlertAction(title: "Copy", style: .default, handler: { (nil) in
+                        UIPasteboard.general.string = object.stringValue
+                    }))
+                    present(alert, animated: true, completion: nil)
+                }
+            }
+        }
+    }
 }
