27033e904a0ba1476da0968adc14e132b4258236
filipmatic <filip.matic.6@gmail.com>
2019-01-02 15:57:48 -0500

    Finalize edits for V1

--- Kontakt/Kontakt/Main/Views/InfoViewController.swift
+++ Kontakt/Kontakt/Main/Views/InfoViewController.swift
@@ -20,19 +20,22 @@ class InfoViewController: UIViewController {
     private let phoneNumberKit = PhoneNumberKit()
     
     private var contactInfoString: String?
+    private var currentTextField: UITextField?
+    private var keyboardActive: Bool = false
     
     @IBOutlet private var myKontaktLabel: UILabel!
     @IBOutlet private var descriptionLabel: UILabel!
     @IBOutlet private var homeButton: UIButton!
     @IBOutlet private var generateButton: UIButton!
     
-    @IBOutlet private var firstName: UITextField!
-    @IBOutlet private var lastName: UITextField!
-    @IBOutlet private var phoneNumber: PhoneNumberTextField!
-    @IBOutlet private var email: UITextField!
+    @IBOutlet private var firstNameTextField: UITextField!
+    @IBOutlet private var lastNameTextField: UITextField!
+    @IBOutlet private var phoneNumberTextField: PhoneNumberTextField!
+    @IBOutlet private var emailTextField: UITextField!
     
     @IBOutlet var kontaktLabelTopConstraint: NSLayoutConstraint!
-    @IBOutlet var generateButtonTopConstraint: NSLayoutConstraint!
+    @IBOutlet var generateButtonBottomConstraint: NSLayoutConstraint!
+    
     @IBOutlet var lastNameTopConstraint: NSLayoutConstraint!
     @IBOutlet var phoneTopConstraint: NSLayoutConstraint!
     @IBOutlet var emailTopConstraint: NSLayoutConstraint!
@@ -43,6 +46,7 @@ class InfoViewController: UIViewController {
         setupView()
         setupHomeButton()
         setTextFieldProperties()
+        loadTextFields()
         
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
@@ -66,6 +70,8 @@ class InfoViewController: UIViewController {
     @objc func keyboardWillShow(_ notification: NSNotification) {
         let keyboardSize = (notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue.size
         
+        keyboardActive = true
+        
         if kontaktLabelTopConstraint.constant != 27 {
             return
         }
@@ -73,41 +79,54 @@ class InfoViewController: UIViewController {
         myKontaktLabel.isHidden = true
         descriptionLabel.isHidden = true
         
-        kontaktLabelTopConstraint.constant = kontaktLabelTopConstraint.constant - (keyboardSize.height - 90)
+        kontaktLabelTopConstraint.constant = -80
+        generateButtonBottomConstraint.constant = generateButtonBottomConstraint.constant + (keyboardSize.height - 30)
+        
+//        kontaktLabelTopConstraint.constant = kontaktLabelTopConstraint.constant - (keyboardSize.height - 90)
         lastNameTopConstraint.constant = lastNameTopConstraint.constant - 15
         phoneTopConstraint.constant = phoneTopConstraint.constant - 15
         emailTopConstraint.constant = emailTopConstraint.constant - 15
-        generateButtonTopConstraint.constant = generateButtonTopConstraint.constant - 20
+//        generateButtonTopConstraint.constant = generateButtonTopConstraint.constant - 20
         view.layoutIfNeeded()
     }
     
     @objc func keyboardWillHide(_ notification: NSNotification) {
         let keyboardSize = (notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue.size
         
+        keyboardActive = true
+        
         myKontaktLabel.isHidden = false
         descriptionLabel.isHidden = false
         
-        kontaktLabelTopConstraint.constant = kontaktLabelTopConstraint.constant + (keyboardSize.height - 90)
+        kontaktLabelTopConstraint.constant = 27
+        generateButtonBottomConstraint.constant = generateButtonBottomConstraint.constant - (keyboardSize.height - 30)
+        
+//        kontaktLabelTopConstraint.constant = kontaktLabelTopConstraint.constant + (keyboardSize.height - 90)
         lastNameTopConstraint.constant = lastNameTopConstraint.constant + 15
         phoneTopConstraint.constant = phoneTopConstraint.constant + 15
         emailTopConstraint.constant = emailTopConstraint.constant + 15
-        generateButtonTopConstraint.constant = generateButtonTopConstraint.constant + 20
+//        generateButtonTopConstraint.constant = generateButtonTopConstraint.constant + 20
         view.layoutIfNeeded()
     }
     
     @IBAction private func homeButtonTapped(_ sender: UIButton) {
+        dismissKeyboard()
         infoDelegate?.infoDidFinishSuccessfully(true)
     }
     
     @IBAction private func generateButtonTapped(_ sender: UIButton) {
-        contactInfoString = "\(firstName.text!),\(lastName.text!),\(phoneNumber.text!),\(email.text!)"
-        UserDefaults.standard.set(firstName.text, forKey: "firstName")
-        UserDefaults.standard.set(lastName.text, forKey: "lastName")
-        UserDefaults.standard.set(email.text, forKey: "email")
-        UserDefaults.standard.set(phoneNumber.text, forKey: "phoneNumber")
+        contactInfoString = "\(firstNameTextField.text!),\(lastNameTextField.text!),\(phoneNumberTextField.text!),\(emailTextField.text!)"
+        UserDefaults.standard.set(firstNameTextField.text, forKey: "firstName")
+        UserDefaults.standard.set(lastNameTextField.text, forKey: "lastName")
+        UserDefaults.standard.set(phoneNumberTextField.text, forKey: "phoneNumber")
+        UserDefaults.standard.set(emailTextField.text, forKey: "email")
+        
+        dismissKeyboard()
         
         if !UserDefaults.standard.bool(forKey: "onboardingCompleted") {
             infoDelegate?.infoDidFinishSuccessfully(false)
+        } else {
+            infoDelegate?.infoDidFinishSuccessfully(true)
         }
         
         UserDefaults.standard.set(true, forKey: "onboardingCompleted")
@@ -125,48 +144,79 @@ class InfoViewController: UIViewController {
         homeButton.tintColor = UIColor.white
     }
     
+    private func dismissKeyboard() {
+        if currentTextField == firstNameTextField {
+            firstNameTextField.resignFirstResponder()
+        } else if currentTextField == lastNameTextField {
+            lastNameTextField.resignFirstResponder()
+        } else if currentTextField == phoneNumberTextField {
+            phoneNumberTextField.resignFirstResponder()
+        } else if currentTextField == emailTextField {
+            emailTextField.resignFirstResponder()
+        }
+    }
+    
+    private func loadTextFields() {
+        if let firstNameFieldString = UserDefaults.standard.string(forKey: "firstName") {
+            firstNameTextField.text = firstNameFieldString
+        }
+        if let lastNameFieldString = UserDefaults.standard.string(forKey: "lastName") {
+            lastNameTextField.text = lastNameFieldString
+        }
+        if let phoneNumberFieldString = UserDefaults.standard.string(forKey: "phoneNumber") {
+            phoneNumberTextField.text = phoneNumberFieldString
+        }
+        if let emailFieldString = UserDefaults.standard.string(forKey: "email") {
+            emailTextField.text = emailFieldString
+        }
+    }
+    
     private func setTextFieldProperties() {
-        firstName.delegate = self
-        firstName.tag = 100
+        firstNameTextField.delegate = self
+        firstNameTextField.tag = 100
         
-        lastName.delegate = self
-        lastName.tag = 200
+        lastNameTextField.delegate = self
+        lastNameTextField.tag = 200
         
-        phoneNumber.delegate = self
-        phoneNumber.tag = 300
+        phoneNumberTextField.delegate = self
+        phoneNumberTextField.tag = 300
         
-        email.delegate = self
-        email.tag = 400
+        emailTextField.delegate = self
+        emailTextField.tag = 400
         
-        firstName.autocorrectionType = .no
-        firstName.autocapitalizationType = .words
-        firstName.setupTextFieldAppearance()
+        firstNameTextField.autocorrectionType = .no
+        firstNameTextField.autocapitalizationType = .words
+        firstNameTextField.setupTextFieldAppearance()
         
-        lastName.autocorrectionType = .no
-        lastName.autocapitalizationType = .words
-        lastName.setupTextFieldAppearance()
+        lastNameTextField.autocorrectionType = .no
+        lastNameTextField.autocapitalizationType = .words
+        lastNameTextField.setupTextFieldAppearance()
         
-        phoneNumber.setupTextFieldAppearance()
+        phoneNumberTextField.setupTextFieldAppearance()
         
-        email.keyboardType = .emailAddress
-        email.autocorrectionType = .no
-        email.autocapitalizationType = .none
-        email.setupTextFieldAppearance()
+        emailTextField.keyboardType = .emailAddress
+        emailTextField.autocorrectionType = .no
+        emailTextField.autocapitalizationType = .none
+        emailTextField.setupTextFieldAppearance()
     }
 }
 
 extension InfoViewController : UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        if textField === firstName {
-            lastName.becomeFirstResponder()
-        } else if textField == lastName {
-            phoneNumber.becomeFirstResponder()
-        } else if textField == phoneNumber {
-            email.becomeFirstResponder()
+        if textField === firstNameTextField {
+            lastNameTextField.becomeFirstResponder()
+        } else if textField == lastNameTextField {
+            phoneNumberTextField.becomeFirstResponder()
+        } else if textField == phoneNumberTextField {
+            emailTextField.becomeFirstResponder()
         } else {
             textField.resignFirstResponder()
         }
         
         return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        self.currentTextField = textField
     }
 }
